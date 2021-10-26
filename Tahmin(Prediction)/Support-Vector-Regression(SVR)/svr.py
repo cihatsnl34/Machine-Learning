7932b1b73a8a095d853bbfba5812b6664cc10752
Cihat Şenel <70061200+cihatsnl34@users.noreply.github.com>
2021-10-26 16:19:16 +0300

    SVR regression oluşturuldu

---
+++ Tahmin(Prediction)/Support-Vector-Regression(SVR)/svr.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 26 16:01:45 2021
+
+@author: cihat
+"""
+
+#1.kutuphaneler
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# veri yukleme
+veriler = pd.read_csv('maaslar.csv')
+
+#Data frame dilimleme (slice)
+x = veriler.iloc[:,1:2]
+y = veriler.iloc[:,2:]
+
+#NumPY dizi(array) dönüşümü
+X = x.values
+Y = y.values
+
+
+#linear regression
+#doğrusal model oluşturma
+
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression()
+lin_reg.fit(X,Y)
+
+
+
+
+#polynomial regression
+#doğrusal olmayan (nonlinear model) oluşturma
+#2.Dereceden polinom
+
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 2)#Polinom Derece
+x_poly = poly_reg.fit_transform(X)
+lin_reg2 = LinearRegression()
+lin_reg2.fit(x_poly,y)
+
+
+
+#4. dereceden polinom
+
+poly_reg3 = PolynomialFeatures(degree = 4)
+x_poly3 = poly_reg3.fit_transform(X)
+lin_reg3 = LinearRegression()
+lin_reg3.fit(x_poly3,y)
+
+#Görselleştirme
+
+plt.scatter(X,Y,color='red')
+plt.plot(x,lin_reg.predict(X), color = 'blue')
+plt.show()
+
+plt.scatter(X,Y,color = 'red')
+plt.plot(X,lin_reg2.predict(poly_reg.fit_transform(X)), color = 'blue')
+plt.show()
+
+
+plt.scatter(X,Y,color = 'red')
+plt.plot(X,lin_reg3.predict(poly_reg3.fit_transform(X)), color = 'blue')
+plt.show()
+
+#tahminler
+
+print(lin_reg.predict([[11]]))
+print(lin_reg.predict([[6.6]]))
+
+print(lin_reg3.predict(poly_reg3.fit_transform([[6.6]])))
+print(lin_reg3.predict(poly_reg3.fit_transform([[11]])))
+
+
+#ZORUNLULUK
+#verilerin ölçeklenmesi aynı dünyaya koyuyoruz verileri
+from sklearn.preprocessing import StandardScaler
+sc1=StandardScaler()
+x_olcekli= sc1.fit_transform(X)
+sc2=StandardScaler()
+y_olcekli= sc2.fit_transform(Y)
+
+from sklearn.svm import SVR
+svr_reg=SVR(kernel='rbf')
+svr_reg.fit(x_olcekli,y_olcekli)
+
+
+plt.scatter(x_olcekli,y_olcekli, color='red')
+plt.plot(x_olcekli,svr_reg.predict(x_olcekli),color='blue')
+
+
