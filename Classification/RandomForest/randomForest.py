13d2df7b93b2859ca7d14b5687b2a126ab5859ed
Cihat Şenel <70061200+cihatsnl34@users.noreply.github.com>
2021-12-09 18:21:31 +0300

    RandomForestClassification created.

---
+++ Classification/RandomForest/randomForest.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 16 23:30:46 2021
+
+@author: cihat
+"""
+#1.kutuphaneler
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt 
+from sklearn.metrics import confusion_matrix
+
+#2.veriOnisleme
+#2.1 veri yükleme
+
+veriler= pd.read_csv("veriler.csv")
+
+
+x=veriler.iloc[:,1:4].values
+y=veriler.iloc[:,4:5].values
+#verilerin eğitim ve test için bölünmesi
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.33,random_state=0)
+
+
+#verilerin ölçeklenmesi aynı dünyaya koyuyoruz verileri
+from sklearn.preprocessing import StandardScaler
+sc=StandardScaler()
+X_train= sc.fit_transform(x_train)
+X_test= sc.fit_transform(x_test)
+
+from sklearn.ensemble import RandomForestClassifier
+rfc=RandomForestClassifier(n_estimators=10,criterion='entropy')
+#default olarak gini fonksiyonunu alır logaritma hesabını
+#yapmadan formule koyar
+#entropy log olarak formüle koyup yapıyor 
+#Sonucu çok etkilemez
+rfc.fit(X_train,y_train)
+y_pred=rfc.predict(X_test)
+
+cm=confusion_matrix(y_test,y_pred)
+print("RFC")
+print(cm)
