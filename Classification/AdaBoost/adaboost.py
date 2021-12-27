84ae0e78506ea2d43ee2ef3e770246078b9aaebe
Cihat Şenel <70061200+cihatsnl34@users.noreply.github.com>
2021-12-27 18:41:41 +0300

    refactory

--- Classification/AdaBoost/adaboost.py
+++ Classification/AdaBoost/adaboost.py
@@ -8,16 +8,25 @@ Created on Sat Oct 16 23:30:46 2021
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt 
-from sklearn.metrics import confusion_matrix
-
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score, f1_score
 #2.veriOnisleme
 #2.1 veri yükleme
 
 veriler= pd.read_csv("veriler.csv")
 
+cinsiyet=veriler.iloc[:,4:5].values
+from sklearn import preprocessing
+#1,2,3 :LabelEncoder() 
+le=preprocessing.LabelEncoder()
+cinsiyet[:,0]=le.fit_transform(veriler.iloc[:,-1])
+print(cinsiyet)
+
+
 
 x=veriler.iloc[:,1:4].values
-y=veriler.iloc[:,4:5].values
+y=cinsiyet
 #verilerin eğitim ve test için bölünmesi
 from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.33,random_state=0)
@@ -29,15 +38,16 @@ sc=StandardScaler()
 X_train= sc.fit_transform(x_train)
 X_test= sc.fit_transform(x_test)
 
-from sklearn.ensemble import RandomForestClassifier
-rfc=RandomForestClassifier(n_estimators=10,criterion='entropy')
-#default olarak gini fonksiyonunu alır logaritma hesabını
-#yapmadan formule koyar
-#entropy log olarak formüle koyup yapıyor 
-#Sonucu çok etkilemez
-rfc.fit(X_train,y_train)
-y_pred=rfc.predict(X_test)
+
+abc=AdaBoostClassifier(DecisionTreeClassifier(max_depth=2),n_estimators=4)
+abc.fit(X_train,y_train)
+y_pred=abc.predict(X_test)
+#CONFUSION MATRIX
+
 
 cm=confusion_matrix(y_test,y_pred)
-print("RFC")
-print(cm)
+print(cm) 
+print("ACC")
+print(accuracy_score(y_test, y_pred))
+print("F2 Score")
+print(f1_score(y_test, y_pred))
